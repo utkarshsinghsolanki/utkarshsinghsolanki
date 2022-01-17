148373ca86a430a63b6f1f34df6213ef0652dea5
Utkarsh Singh <29singhutkarsh2907@gmail.com>
2022-01-17 13:15:48 +0530

    Updated

---
+++ files/script.js
@@ -0,0 +1,17 @@
+
+document.querySelector('.menu-btn').addEventListener("click", () => {
+    document.querySelector('.nav').classList.toggle('nav-go')
+    let btn = document.querySelector('.menu-btn')
+    if (document.querySelector('.nav').classList.contains('nav-go')) {
+        btn.style.transform = 'rotate(0deg)'
+    }
+    else {
+        btn.style.transform = 'rotate(180deg)'
+    }
+})
+
+let loader = document.querySelector('.preloader')
+function preloader() {
+    loader.style.display = "none";
+}
+
