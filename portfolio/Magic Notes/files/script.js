841bb06db431284566589c0c827f2adcb7d0d1e8
Utkarsh Singh <29singhutkarsh2907@gmail.com>
2022-02-18 15:47:23 +0530

    Updated

--- portfolio/Magic Notes/files/script.js
+++ portfolio/Magic Notes/files/script.js
@@ -1,4 +1,4 @@
-console.log('Magic Notes');
+// Declaring Variable
 let text;
 let addbtn = document.querySelector('.add-btn');
 let notediv = document.querySelector('.notes-container');
@@ -7,15 +7,18 @@ let noteobj ;
 let html;
 let heading;
 let noteheadobj;
+displayNote();
+// Adding Event listener
 addbtn.addEventListener('click', function addnote(){
-    heading = document.getElementById('note-heading').value;
-    text = document.getElementById('note').value;
-    noteheading = localStorage.getItem('heading')
-    note = localStorage.getItem('notes');
-    console.log(note);
+    heading = document.getElementById('note-heading').value;       //Text that user has enterd in heading
+    text = document.getElementById('note').value;                  //Text that user has enterd in notes
+    noteheading = localStorage.getItem('heading');                 //Getting Heading From Local Storage
+    note = localStorage.getItem('notes');                          //Getting Text From Local Storage
+//If user has not enter the value
     if(note == null){
         noteobj = [];
     }
+//If user has enter the value
     else{
         noteobj = JSON.parse(note);
     }
@@ -25,8 +28,10 @@ addbtn.addEventListener('click', function addnote(){
     else{
         noteheadobj = JSON.parse(noteheading);
     }
+// Pushing values to variable
     noteheadobj.push(heading);
     noteobj.push(text);
+// To display error message when user has not enter any value
     if(text.length == 0){
         noteobj.pop();
         alert('Cannot Create Empty Note');
@@ -35,19 +40,26 @@ addbtn.addEventListener('click', function addnote(){
         noteheadobj.pop();
         alert('Cannot Create Note Without Heading');
     }
+// Uploading content to localStorage
     localStorage.setItem('heading', JSON.stringify(noteheadobj));
     localStorage.setItem('notes', JSON.stringify(noteobj));
+// After Uploading setting input area value back to null
     document.getElementById('note').value = '';
     document.getElementById('note-heading').value = '';
-    showNote();
+// Run function showNotes
+    displayNote();
 });
-showNote();
-function showNote(){
-    noteheading = localStorage.getItem('heading');
-    note = localStorage.getItem('notes');
+
+// displayNote() Function
+function displayNote(){
+    noteheading = localStorage.getItem('heading');              //Getting Heading From Local Storage
+    note = localStorage.getItem('notes');                       //Getting Text From Local Storage
+// If user has not enter the value
     if (note == null) {
         noteobj = [];
-      } else {
+    } 
+// If user has enter the value
+    else {
         noteobj = JSON.parse(note);
     }
     if(noteheading == null){
@@ -56,8 +68,10 @@ function showNote(){
     else{
         noteheadobj = JSON.parse(noteheading);
     }
-    html = '';
+    html = '';                                                  //Declaring html as an empty string
+// Running a forEach loop for noteobj so every element in an array could be displayed
     noteobj.forEach(function(element,index){
+        // Declaring html to a html code
         html += `<div class="notes">
                         <h3>${noteheadobj[index].toUpperCase()}</h3>
                         <br>
@@ -65,20 +79,29 @@ function showNote(){
                         <br><br>
                         <button onclick="deleteNote(this.id)" id="${index}" >Delete Note</button>
                     </div>`
-                });
-    if(noteobj.length ==0){
-        notediv.innerText =  `Nothing to display here. Click on Add Note to create one.`
+    });
+// If user has not created any note before it will display a message.
+    if(noteobj.length==0){
+        notediv.innerText =  `Nothing to display here. Click on Add Note to create one.`;
     }
+// If user already has created a note before it will add html code inside notediv
     else{
         notediv.innerHTML = html;
     }
 }
+
+
+// deleteNote() Function
 function deleteNote(index){
-   let note = localStorage.getItem('notes');
-   if (note==null){
-       noteobj = [];
-   }
-   else{
+
+    noteheading = localStorage.getItem('heading');              //Getting Heading From Local Storage
+    note = localStorage.getItem('notes');                       //Getting Text From Local Storage
+// If user has not enter the value
+    if (note==null){
+        noteobj = [];
+    }
+// If user has enter the value
+    else{
        noteobj= JSON.parse(note);
     }
     if(noteheading == null){
@@ -87,16 +110,21 @@ function deleteNote(index){
     else{
         noteheadobj = JSON.parse(noteheading);
     }
+// To delete a particular element from an array
     noteheadobj.splice(index, 1);
     noteobj.splice(index, 1);
+// Updating localStorage and running displayNote() function to display changes.
     localStorage.setItem('heading', JSON.stringify(noteheadobj));
     localStorage.setItem('notes', JSON.stringify(noteobj));
-    showNote();
+    displayNote();
 }
 
-let search = document.querySelector('.search-area');
-search.addEventListener("input", function(){
 
+// Giving logic to search bar
+let search = document.querySelector('.search-area');
+// Adding event listener to searchbar
+search.addEventListener("input", function(){
+// Declaring Variables
     let inputVal = search.value.toLowerCase();
     let noteCards = document.getElementsByClassName('notes');
     Array.from(noteCards).forEach(function(element){
