80323138029eb0e39282e42d9f3dcbf3e3f9116c
Utkarsh Singh <29singhutkarsh2907@gmail.com>
2022-03-27 13:20:54 +0530

    v 1.1.0

--- files/script.js
+++ files/script.js
@@ -1,22 +1,31 @@
-let btn = document.querySelector('.menu-btn')
-document.querySelector('.menu-btn').addEventListener("click", () => {
-    document.querySelector('.nav').classList.toggle('nav-go')
-    if (document.querySelector('.nav').classList.contains('nav-go')) {
-        btn.style.transform = 'rotate(0deg)'
+let btn = document.querySelector(".menu-btn");
+document.querySelector(".menu-btn").addEventListener("click", () => {
+  document.querySelector(".nav").classList.toggle("nav-go");
+  if (document.querySelector(".nav").classList.contains("nav-go")) {
+    btn.style.transform = "rotate(0deg)";
+  } else {
+    btn.style.transform = "rotate(180deg)";
+  }
+});
 
-    }
-    else {
-        btn.style.transform = 'rotate(180deg)';
-    }
-})
-
-let loader = document.querySelector('.preloader')
+let loader = document.querySelector(".preloader");
 function preloader() {
-    loader.style.display = "none";
+  loader.style.display = "none";
 }
 
-function navgo(){
-    document.querySelector('.nav').classList.add('nav-go')
-    btn.style.transform = 'rotate(0deg)'
-    
+function navgo() {
+  document.querySelector(".nav").classList.add("nav-go");
+  btn.style.transform = "rotate(0deg)";
 }
+
+document.getElementById("searchBar").addEventListener("input", () => {
+  let value = document.getElementById("searchBar").value.toLowerCase();
+  let card = document.getElementsByClassName("project-btn");
+  Array.from(card).forEach((element) => {
+    if (element.innerText.toLowerCase().includes(value)){
+      element.style.display = "block";
+    } else{
+      element.style.display = "none";
+    }
+  });
+});
